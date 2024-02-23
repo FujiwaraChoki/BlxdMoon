6dff010a7bb456dd51d0d4b197f3e7d125bdb2e6
FujiwaraChoki <sami@samihindi.com>
2024-02-23 11:08:47 +0100

    Use Header Files appropriately

--- include/status.h
+++ include/status.h
@@ -1,23 +1,10 @@
 // Status functions (success, error, warning, info), including colors
-
 #include <stdio.h>
 
-void success(const char *msg)
-{
-  printf("\033[1;32m%s\033[0m", msg);
-}
+void success(const char *msg);
 
-void error(const char *msg)
-{
-  printf("\033[1;31m%s\033[0m\n", msg);
-}
+void error(const char *msg);
 
-void warning(const char *msg)
-{
-  printf("\033[1;33m%s\033[0m\n", msg);
-}
+void warning(const char *msg);
 
-void info(const char *msg)
-{
-  printf("\033[1;34m%s\033[0m\n", msg);
-}
+void info(const char *msg);
---
+++ src/status.c
@@ -0,0 +1,21 @@
+#include "status.h"
+
+void success(const char *msg)
+{
+  printf("\033[1;32m%s\033[0m", msg);
+}
+
+void error(const char *msg)
+{
+  printf("\033[1;31m%s\033[0m\n", msg);
+}
+
+void warning(const char *msg)
+{
+  printf("\033[1;33m%s\033[0m\n", msg);
+}
+
+void info(const char *msg)
+{
+  printf("\033[1;34m%s\033[0m\n", msg);
+}
